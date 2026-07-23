542adc1941a22349add6a0365517aec0f6ffb209
itsritesh02 <itsritesh02@gmail.com>
2026-07-23 15:55:26 +0530

    best time to sell buhy

---
+++ Array/BestTimeToSellBuy.java
@@ -0,0 +1,24 @@
+public class BestTimeToSellBuy {
+  static int profitMax(int prices[]) {
+    int min = prices[0];
+    int profit = 0;
+    for (int i = 1; i < prices.length; i++) {
+      if (prices[i] < min) {
+        min = prices[i];
+      }
+      if (prices[i] - min > profit) {
+        profit = prices[i] - min;
+      }
+
+    }
+    return profit;
+  }
+
+  public static void main(String[] args) {
+
+    int[] prices = { 5, 2, 8, 1, 9, 4 };
+
+    System.out.println("Maximum profit: " + profitMax(prices));
+
+  }
+}
--- Array/TwoSum.java
+++ Array/TwoSum.java
@@ -1,6 +1,6 @@
-class Solution {
-  static void TwoSum(int[] arr) {
-    int target = 19;
+class TwoSum {
+  static void Sum(int[] arr) {
+    int target = 11;
     for (int i = 0; i < arr.length; i++) {
       for (int j = i + 1; j < arr.length; j++) {
         int ans = arr[i] + arr[j];
@@ -17,6 +17,6 @@ class Solution {
 
   public static void main(String[] args) {
     int[] arr = { 4, 2, 5, 8, 6, 1, 3, 7 };
-    TwoSum(arr);
+    Sum(arr);
   }
 }
